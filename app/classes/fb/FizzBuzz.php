89f9bd6534e95da357aa5722e30582b82b2ebf9b
新倉23 <r-niikura@c-tos.com>
2015-11-11 19:05:26 +0900

    処理完了

--- app/classes/fb/FizzBuzz.php
+++ app/classes/fb/FizzBuzz.php
@@ -8,44 +8,44 @@ use App\fb\Screamer;
  */
 class FizzBuzz
 {
-    
+
     private $screamers = [];
-    
+
     /**
      * 叫び声を上げるオブジェクトを追加する
-     * 
+     *
      * @param Screamer $obj スクリーマーオブジェクト
-     * 
+     *
      * @return void
      */
     public function setScreamer(Screamer $obj)
     {
         $this->screamers[] = $obj;
-        
+
     }
-    
+
     /**
      * 現在のスクリーマーの個数を出力する
-     * 
+     *
      * テスト用に作成したもの
-     * 
+     *
      * @return integer
      */
     public function getCount()
     {
         return count($this->screamers);
-        
+
     }
-    
+
     /**
      * FizzBuzzのメインメソッド
-     * 
+     *
      * スクリーマーに叫ぶかどうかのチェックをさせ、
      * 叫ぶ対象で合ったら叫ばせる
      * 叫ばない場合は数字をそのまま返す
-     * 
+     *
      * @param integer $number 数値
-     * 
+     *
      * @return integer|string
      */
     public function run($number)
@@ -56,8 +56,8 @@ class FizzBuzz
                 $str .= $obj->shout();
             }
         }
-        
+
         return ($str === '') ? $number : $str;
-        
+
     }
 }
