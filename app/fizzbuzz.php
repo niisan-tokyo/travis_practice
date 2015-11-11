27c4a20f0002baa92ff53ca5e07f650f2947c591
新倉23 <r-niikura@c-tos.com>
2015-11-11 19:09:42 +0900

    スクリプトコマンド作成

--- app/fizzbuzz.php
+++ app/fizzbuzz.php
@@ -1,4 +1,16 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
 
-echo 'ok';
+$number = intval($argv[1]);
+
+$main = new App\fb\FizzBuzz();
+$main->setScreamer(new \App\fb\screamer\Fizz());
+$main->setScreamer(new \App\fb\screamer\Buzz());
+
+echo 'start====================================' . PHP_EOL;
+
+for ($num = 1; $num <= $number; $num++) {
+    echo $main->run($num) . PHP_EOL;
+}
+
+echo 'end======================================' . PHP_EOL;
